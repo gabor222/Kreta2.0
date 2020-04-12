a676497176b815d7d184a777ce2ed6f4a4581e2d
gabor222 <gabor222@gmail.hu>
2020-04-12 21:25:34 +0200

    CI

--- backend/src/test/java/hu/elte/alkfejl/enaplo/EnaploApplicationTests.java
+++ backend/src/test/java/hu/elte/alkfejl/enaplo/EnaploApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EnaploApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void fakeTest() {
+		long i = 1;
+		assertEquals(1, i);
 	}
-
 }
